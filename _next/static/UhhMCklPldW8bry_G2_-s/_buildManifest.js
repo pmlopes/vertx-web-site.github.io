96d455373749bbb3157b59ec494e4cfa6668566d
michel-kraemer <michel-kraemer@users.noreply.github.com>
2020-03-26 15:36:47 +0000

    deploy: vertx-web-site/vertx-web-site@707a81071ceb9f50517c2f300df368f33157719c

--- _next/static/UhhMCklPldW8bry_G2_-s/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/get-started":[a],"/index":[a]}}("static\u002Fchunks\u002F4cb748a8b7b8317c55e58ee3f8561c24a1e0fa7b.7bdc92fdc2adb1d39a14.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
